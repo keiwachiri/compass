ec876371078091605ae9cdd35d6afaa268ba1772
keiwachiri <keiwachiri@gmail.com>
2016-07-08 14:37:50 +0200

    Add password-reset methods to User model + tests

--- tests/auth_tests/models_tests.py
+++ tests/auth_tests/models_tests.py
@@ -49,9 +49,9 @@ class UserTest(unittest.TestCase):
         self.assertNotEqual(u2.password_hash, self.user.password_hash)
 
     # Confirmation-token testing
-    def test_generate_confirmatin_token_returns_byte_string(self):
+    def test_generate_confirmation_token_returns_byte_string(self):
         token = self.user.generate_confirmation_token()
-        self.assertTrue(type(token), bytes)
+        self.assertEqual(type(token), bytes)
 
     def test_confirm_returns_true_on_correct_token_and_confirms_user(self):
         token = self.user.generate_confirmation_token()
@@ -72,6 +72,30 @@ class UserTest(unittest.TestCase):
         time.sleep(2)
         self.assertFalse(self.user.confirm(token))
         self.assertFalse(self.user.confirmed)
+
+    # Reset-password testing
+    def test_generate_reset_token_returns_byte_string_object(self):
+        token = self.user.generate_reset_token()
+        self.assertEqual(type(token), bytes)
+
+    def test_reset_password_changes_password_with_right_token(self):
+        token = self.user.generate_reset_token()
+        self.assertTrue(self.user.reset_password(token, 'new_password'))
+        self.assertTrue(self.user.verify_password('new_password'))
+
+    def test_reset_password_doesnt_change_password_on_wrong_token(self):
+        u2 = User(username='user2', password='pass', email='example2@mail.com')
+        db.session.add(u2)
+        db.session.commit()
+        token = u2.generate_reset_token()
+        self.assertFalse(self.user.reset_password(token, 'new_password'))
+        self.assertFalse(self.user.verify_password('new_password'))
+
+    def test_reset_password_doesnt_change_password_when_token_expired(self):
+        token = self.user.generate_reset_token(expiration=1)
+        time.sleep(2)
+        self.assertFalse(self.user.reset_password(token, 'new_password'))
+        self.assertFalse(self.user.verify_password('new_password'))
 
 
 if __name__ == "__main__":
